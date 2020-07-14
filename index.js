3d0c47887e32455717adc82b743077eb8d482b61
Resha Puspita <reshapuspita734@gmail.com>
2020-07-14 17:46:43 +0700

    add tampilan

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// const ejs = require("ejs");
+const ejs = require("ejs");
 
 const { PORT } = require("./config");
 const localPort = PORT || 5000;
@@ -9,19 +9,16 @@ const db = require("./config");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/products", require("./routes/products"));
 // // Routes
-// app.get("/", (req, res) => {
-//   const results = ...
-//   res.render("pages/home", {
-//     results,
-//   });
-// });
+app.get("/", (req, res) => {
+  res.render("pages/home");
+});
+app.use("/products", require("./routes/products"));
 
-// // Membaca file .ejs
-// app.set("view engine", "ejs");
+// Membaca file .ejs
+app.set("view engine", "ejs");
 
-// // membaca file statis di folder views
+// membaca file statis di folder views
 // app.use(express.static("views"));
 
 if (db) {
