4341c084d41fe4dd04e4b4302b9456743184e9b9
Resha Puspita <reshapuspita734@gmail.com>
2020-07-14 17:13:48 +0700

    add views

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const ejs = require("ejs");
+// const ejs = require("ejs");
 
 const { PORT } = require("./config");
 const localPort = PORT || 5000;
@@ -9,20 +9,20 @@ const db = require("./config");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Routes
-app.get("/", (req, res) => {
-  const results = ... 
-  res.render("pages/home", {
-    results,
-  });
-});
 app.use("/products", require("./routes/products"));
+// // Routes
+// app.get("/", (req, res) => {
+//   const results = ...
+//   res.render("pages/home", {
+//     results,
+//   });
+// });
 
-// Membaca file .ejs
-app.set("view engine", "ejs");
+// // Membaca file .ejs
+// app.set("view engine", "ejs");
 
-// membaca file statis di folder views
-app.use(express.static("views"));
+// // membaca file statis di folder views
+// app.use(express.static("views"));
 
 if (db) {
   app.listen(localPort, (req, res) => {
